93690c7815d9a8c2ca0f63c17b0755dd4c074df0
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 07:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,9 +38,7 @@ var routine = [
   ]
   
   function exerciseDog(dogName, dogBreed){
-    for(let i = 0; i < routine.length - 1; i++){
-      routine[i]
-    }
+    return routine.map(b => b(dog, breed))
     
   }
     
