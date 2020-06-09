356206f4020b7f221e01a190cf87ffb0756ead38
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 07:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var routine = [
   
   function exerciseDog(dogName, dogBreed){
     for(let i = 0; i < routine.length - 1; i++){
-      this(dogName, dogBreed)[i]
+      routine[i](dogName, dogBreed)
     }
     
   }
